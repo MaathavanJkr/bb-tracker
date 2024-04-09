36092755e58789e7c41d360ff861e0a95bfb8191
MaathavanJkr <MaathavanJkr@users.noreply.github.com>
2024-04-09 06:35:59 +0000

    deploy: b4d9159f56bd03f0d971fc9e3b6df59b57f901db

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bb-tracker/precache-manifest.05efb663bf079896a580b7aefd8d517c.js"
+  "/precache-manifest.224c3a499a0ba513ae6af0df3cfd7cbc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bb-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
